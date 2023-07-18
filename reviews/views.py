adbb932c63c100eeeeefd469c96a42aa92fcbe4d
runyud <runyud@umich.edu>
2023-07-17 21:56:32 -0400

    finished django views, modelforms, and forms

--- reviews/views.py
+++ reviews/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views import View
+
+from .forms import ReviewForm
+from .models import Review
 # Create your views here.
 
-def review(request):
-    if request.method == "POST":
-        entered_username = request.POST['username']
-        print(entered_username)
-        return HttpResponseRedirect("/thank-you")
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+    
+    def post(self, request):
+        form = ReviewForm(request.POST)
 
-    return render(request, "reviews/review.html")
-
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/thank-you")
+        
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
 
 def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+    return render(request, "reviews/thank_you.html", {
+        "has_error": False
+    })
